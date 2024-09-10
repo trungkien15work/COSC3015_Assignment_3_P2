aa119ba7df094e966863e9d93c3a63cf442970aa
Ha Hong <janechoi2001@gmail.com>
2024-09-10 21:50:10 +0700

    Update UI and create Search bar

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os, sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from gensim.models import Word2Vec
 import joblib
 from utils import genVec, tokenizeTxt
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobs.db'
@@ -12,19 +14,21 @@ db = SQLAlchemy(app)
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    webIndex = db.Column(db.String(100), nullable=False)
+    company = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    category = db.Column(db.String(50), nullable=False)
-    salary = db.Column(db.String(50), nullable=True)
+    label = db.Column(db.String(50), nullable=False)
+    
 
 @app.route('/')
 def index():
-    category_filter = request.args.get('category')
-    if category_filter:
-        jobs = Job.query.filter_by(category=category_filter).all()
+    label_filter = request.args.get('label')
+    if label_filter:
+        jobs = Job.query.filter_by(label=label_filter).all()
     else:
         jobs = Job.query.all()
-    categories = ['Engineering', 'Healthcare Nursing', 'Accounting Finance', 'Sales']
-    return render_template('index.html', jobs=jobs, categories=categories, selected_category=category_filter)
+    categories = ['Engineering', 'Healthcare_Nursing', 'Accounting_Finance', 'Sales']
+    return render_template('index.html', jobs=jobs, categories=categories, selected_label=label_filter)
 
 @app.route('/job/<int:job_id>')
 def job_detail(job_id):
@@ -33,14 +37,14 @@ def job_detail(job_id):
 
 @app.route('/create', methods=['GET', 'POST'])
 def create_job():
-    categories = ['Engineering', 'Healthcare Nursing', 'Accounting Finance', 'Sales']
+    categories = ['Engineering', 'Healthcare_Nursing', 'Accounting_Finance', 'Sales']
     if request.method == 'POST':
         # Classify the content
         if request.form['button'] == 'Classify':
             # Read the content
             f_title = request.form['title']
             f_content = request.form['description']
-            f_salary = request.form['salary']
+            f_company = request.form['company']
             f_category = request.form['category']
             
             # Tokenize the content of the .txt file so as to input to the saved mode
@@ -63,20 +67,46 @@ def create_job():
             y_pred = model.predict(X_tfidf_des)
             y_pred = y_pred[0]
             print('category', f_category)
-            return render_template('create_job.html', prediction=y_pred, title=f_title, description=f_content, categories=categories, salary=f_salary, selectedCategory=f_category)
+            return render_template('create_job.html', prediction=y_pred, title=f_title, description=f_content, categories=categories, company=f_company, selectedCategory=f_category)
         elif request.form['button'] == 'Save':
             title = request.form['title']
             description = request.form['description']
             category = request.form['category']
-            salary = request.form['salary']
+            company = request.form['company']
             
-            new_job = Job(title=title, description=description, category=category, salary=salary)
+            new_job = Job(title=title, description=description, category=category, company=company)
             db.session.add(new_job)
             db.session.commit()
         
         return redirect(url_for('index'))
     return render_template('create_job.html', categories=categories, selectedCategory="Engineering")
 
+
+@app.route('/search', methods = ['POST'])
+def search():
+    if request.method == 'POST': # If user search
+        folder_path = 'instance'
+        db_path = os.path.join(folder_path, 'jobs.db')
+        conn = sqlite3.connect(db_path) # Get the data from jobs.db
+        conn.row_factory = sqlite3.Row # Make sure it in row format
+        cursor = conn.cursor()
+        if request.form['search'] == '': # If the user click search
+            search_string = request.form["searchword"] # Get the string from search
+            # Execute the SQL query to get the data likely with the searching string
+            cursor.execute('''
+                           SELECT * 
+                           FROM job
+                           WHERE title LIKE ? OR description LIKE ? OR company LIKE ? OR label LIKE ?
+                            ''', (f'%{search_string}%', f'%{search_string}%', f'%{search_string}%', f'%{search_string}%'))
+            # Get all the jobs from the execute SQL query
+            jobs = cursor.fetchall()
+            conn.close()
+
+            return render_template('search.html', jobs = jobs) # Get to the search.html with all the jobs similar with searching string
+   
+    else: # Else go to home page
+        return render_template('home.html')
+    
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
